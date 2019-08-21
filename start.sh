89a448b761ccf07a50a1357ac0fa434e55ac4dc0
zim96 <abdhazim96@gmail.com>
2019-08-21 11:19:35 +0800

    Minor changes to start.sh and added requirements

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-source pipy/bin/activate
-gunicorn --workers 1 --bind unix:pipy.sock -m 007 src:app
+source PiPyEnv/bin/activate
+gunicorn --workers 1 --bind unix:pipy.sock -m 007 src:server
 deactivate
